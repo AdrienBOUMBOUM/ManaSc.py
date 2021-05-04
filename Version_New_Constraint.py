51a690afeae44c4254e18627f3acc9b43565b25d
AdrienBOUMBOUM <83536099+AdrienBOUMBOUM@users.noreply.github.com>
2021-05-04 12:58:01 +0200

    Update Version_New_Constraint.py

--- Version_New_Constraint.py
+++ Version_New_Constraint.py
@@ -1,9 +1,12 @@
+#CarbuVert+FH
+
 from pulp import *
 import pandas as pd
-import numpy as np
 
 
-df = pd.read_excel("Classeur.xlsx", nrows=11)
+### DATA ###
+
+df = pd.read_excel("Data_Silo.xlsx", nrows=12)
 
 silo = list(df['Silo'])
 
@@ -12,13 +15,14 @@ density = dict(zip(silo, df['density']))
 dommage = dict(zip(silo, df['dommage']))
 non_organic = dict(zip(silo, df['non-organic']))
 quantity = dict(zip(silo, df['quantity']))
-cost = dict(zip(silo, df['cost']))
+
 print(silo, humidity, density, dommage, non_organic, quantity)
 
-# DATA CLIENTS #
-df = pd.read_excel("Data_Clients.xlsx", nrows=3)
+### DATA CLIENTS ###
 
-clients = np.arange(3)
+df = pd.read_excel("Data_Clients1.xlsx", nrows=3)
+
+clients = list(df['Client'])
 
 c_humidity = dict(zip(clients, df['humidity']))
 c_density = dict(zip(clients, df['density']))
@@ -29,65 +33,56 @@ c_quantitymax = dict(zip(clients, df['Qmax']))
 
 print(clients, c_density, c_dommage, c_non_organic, c_quantitymax, c_quantitymin, c_humidity)
 
-m = LpProblem(sense=LpMaximize)
+### MODEL ###
 
-###CREATION DES VARIABLES
+model = LpProblem(sense=LpMaximize)
 
-x = [[LpVariable("Client " + str(i) + " Silo " + str(j),lowBound=0) for j in range(11)] for i in range(3)]
-Y = [0] + [LpVariable("Client " + str(i) + " A LA FH",lowBound=0) for i in range(2)]
+x = [[LpVariable("Client " + str(i) + " Silo " + str(j),lowBound=0) for j in range(12)] for i in range(3)]
 
-###CONTRAINTES
+R = 67
 
-# Contraintes clients
-tot_i = {}
-for i in clients:#(3)
-    tot_x = 0
-    for j in silos: #(12)
-        if j == len(silos):
-            tot_x += X[i, j]
-            tot_i[i] = tot_x
+#prix demandé a chaque client
+for i in clients
+    p[i] = (R * 1.15) - (0.5 * c_humidity[i]) - c_dommage[i] - c_non_organic[i] + (5 * c_density[i])
+
+
+
+### CONSTRAINTS ###
+### Contrainte de positivité du blé dans les silos ###
+for j in silo:
+    model += lpSum([X[i, j] for i in clients]) <= quantity[j]
+##Carbu vert pas livré par FH a cause des dates
+x[1, 12] = 0
+
+#contrainte clients
+
+# Contraintes quantités demandées
+for i in clients:
+    model += lpSum([X[i, j] for j in silo]) >= c_quantitymin[i]
+    model += lpSum([X[i, j] for j in silo]) <= c_quantitymax[i]
+
+tot_c = {} #total
+for i in clients:
+    tot_s = 0 #qté que les clients recoivent de chaque silo
+    for j in silo:
+        if j == len(silo):
+            tot_s += X[i, j]
+            tot_c[i] = tot_s
             break
         else:
             pass
-        tot_x += X[i, j]
+        tot_s += X[i, j]
 
 for i in clients:
-    prob += lpSum([X[i, j] * humidity[j] for j in silos]) <= c_hummidity[i] * tot_i[i]
-    prob += lpSum([X[i, j] * dammage[j] for j in silos]) <= c_dammage[i] * tot_i[i]
-    prob += lpSum([X[i, j] * non_organic[j] for j in silos]) <= c_non_organic[i] * tot_i[i]
-    prob += lpSum([X[i, j] * density[j] for j in silos]) >= c_density[i] * tot_i[i]
-
-p = np.zeros(3)
-for i in range(3):
-        p[i] = 67*1.15-0.5*c_humidity[i]-c_dommage[i]-c_non_organic[i] + 5*c_density[i]
-
-### FONCTION OBJECTIF 1
-
-"""
-m += lpDot([lpSum(x[0]),lpSum(x[1]),lpSum(x[2])],p) - lpSum([lpDot(x[0],cost),lpDot(x[1],cost),lpDot(x[2],cost)]) - 60.5 * lpSum(Y)
-"""
-
-### FONCTION OBJECTIF 2
-"""
-m += lpDot([lpSum(x[0]),lpSum(x[1]),lpSum(x[2])],p) - lpSum([lpDot(x[1],cost),lpDot(x[2],cost)]) - 60.5 * lpSum(Y)
-"""
-
-### FONCTION OBJECTIF 3
-"""
-m += lpDot([lpSum(x[0]),lpSum(x[1]),lpSum(x[2])],p) - lpSum([lpDot(x[0],cost),lpDot(x[1],cost),lpDot(x[2],cost)])
-
-"""
-
-### FONCTION OBJECTIF 4
-
-m += lpDot([lpSum(x[0]),lpSum(x[1]),lpSum(x[2])],p) - lpSum([lpDot(x[1],cost),lpDot(x[2],cost)])
-
-
-### RESOLUTION
-m.resolve()
+    model += lpSum([X[i, j] * humidity[j] for j in silos]) <= c_humidity[i] * tot_c[i]#contrainte Humidi
+    model += lpSum([X[i, j] * dommage[j] for j in silos]) <= c_dommage[i] * tot_c[i]#contrainte endommage
+    model += lpSum([X[i, j] * non_organic[j] for j in silos]) <= c_non_organic[i] * tot_c[i]#contrainte non organique
+    model += lpSum([X[i, j] * density[j] for j in silos]) >= c_density[i] * tot_c[i]#contrainte densite
+    
+model.resolve()
 client = ""
 for i in range(3):
-        for j in range(11):
+        for j in range(12):
                 x[i][j] = value(x[i][j])
                 if i == 0 :
                         client = "CarbuVert"
@@ -100,4 +95,3 @@ for i in range(3):
 
                 print("Le silo {}, a vendu {} au client {}".format(j+1,x[i][j],client))
 
-        print("La FH a vendu {} au client {}".format(value(Y[i]),client))
